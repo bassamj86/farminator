fe0e09d80ba33aaface89b47c8f493387cbe1eaa
bassamj86 <123335254+bassamj86@users.noreply.github.com>
2023-08-01 00:15:16 +0500

    Update uploadtodatabase.py

--- uploadtodatabase.py
+++ uploadtodatabase.py
@@ -1,51 +1,12 @@
 import streamlit_authenticator as stauth
 import streamlit as st
 
-def insert_user_data(username, name, hashed_password):
-    db.insert_user(username, name, hashed_password)
-
 
 import streamlit as st
 import sqlite3
 
 
-import os
 
-from deta import Deta  # pip install deta
-
-
-
-# Load the environment variables
-DETA_KEY = "d0k4fws8gi7_xKE62N6R1cmQ9C9dKdHoEqpCPY6yfRN3"
-deta = Deta(DETA_KEY)
-
-db = deta.Base("users")
-
-
-def insert_user(username, name, password):
-    """Returns the user on a successful user creation, otherwise raises and error"""
-    return db.put({"key": username, "name": name, "password": password})
-
-
-def fetch_all_users():
-    """Returns a dict of all users"""
-    res = db.fetch()
-    return res.items
-
-
-def get_user(username):
-    """If not found, the function will return None"""
-    return db.get(username)
-
-
-def update_user(username, updates):
-    """If the item is updated, returns None. Otherwise, an exception is raised"""
-    return db.update(updates, username)
-
-
-def delete_user(username):
-    """Always returns None, even if the key does not exist"""
-    return db.delete(username)
 
 def create_table():
     conn = sqlite3.connect('data.db')
