97a410ea3dfe1a595a9f431283f4a08e6978c450
bassamj86 <123335254+bassamj86@users.noreply.github.com>
2023-08-02 06:47:16 +0500

    Update uploadtodatabase.py

--- uploadtodatabase.py
+++ uploadtodatabase.py
@@ -12,7 +12,34 @@ from deta import Deta
 import streamlit as st
 import bcrypt
 from passlib.context import CryptContext
+import base64
+
+
+
+
+
+
+
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+def get_img_as_base64(file):
+        with open(file, "rb") as f:
+            data = f.read()
+        return base64.b64encode(data).decode()
+
+img = get_img_as_base64("preview.jpg")
+
+
+st.set_page_config(page_title="Admin", page_icon=img, layout="centered", initial_sidebar_state="auto", menu_items=None)
+
+
+
+
+
+
+
+
 
 
 # Load the environment variables
@@ -35,124 +62,179 @@ def local_css(filename):
     with open(filename) as f:
         st.markdown(f"<style>{f.read()}</style>",unsafe_allow_html=True)
 
-
-
-
-
 local_css('admin.css')
 
-selected3 = option_menu(None, ["User Registration", "User Chart Add"],
-     
-     default_index=0, orientation="horizontal",
-    styles = {
-    "container": {
-        "padding": "1rem 2rem",
-        "background-color": "white",
-        "border-radius": "10px",
-        "box-shadow": "0 0 10px rgba(0, 0, 0, 0.1)",
-        "font-color": "white"
-    },
-    "icon": {
-        "color": "white",
-        "font-size": "25px",
-        "display": "none",
-        "font-color": "white"
-        
-    },
-    "nav-link": {
-        "font-size": "18px",
-        "font-color": "white",
-        "text-align": "left",
-        "padding": "0.5rem",
-        "margin": "0",
-        "border-radius": "5px",
-        "transition": "background-color 0.3s",
-        "--hover-color": "#e0e0e0",
-        "cursor": "pointer"
-    },
-    "nav-link-selected": {
-        "background-color": "#9C2222",
-        "color": "white",
-        "font-weight": "bold"
-    },
-}
-    
-)
 
+DETA_KEY_ADMIN = "d0k4fws8gi7_xKE62N6R1cmQ9C9dKdHoEqpCPY6yfRN3"
+deta = Deta(DETA_KEY_ADMIN)
 
-def hash_password(password):
-    # Using hashlib to hash the password securely
-    return hashlib.sha256(password.encode()).hexdigest()
+db_admin = deta.Base("admincredentials")
 
-def insert_user(username, name, password):
-    """Returns the user on a successful user creation, otherwise raises and error"""
-    return db_register.put({"key": username, "name": username, "password": password})
-
-def fetch_all_users():
-
-    res = db_register.fetch()
+def fetch_admin():
+    """Returns a dict of all users"""
+    res = db_admin.fetch()
     return res.items
 
 
-DETA_KEY_REGISTER = "d0k4fws8gi7_xKE62N6R1cmQ9C9dKdHoEqpCPY6yfRN3"
-deta_register = Deta(DETA_KEY_REGISTER)
-db_register = deta_register.Base("users")
-def insert_user(username, name, password):
-    """Returns the user on a successful user creation, otherwise raises and error"""
-    return db_register.put({"key": username, "name": name, "password": password})
-
-users = fetch_all_users()
-un = [user["key"] for user in users]
 
 
-if selected3 == "User Registration":
-    
 
-    usernames = st.text_input("Username", key=9)
-    names = st.text_input("Name", key=10)
-    passs = st.text_input("Password", type="password", key=11)
 
-    if st.button("Save"):
+
+def delete_user(username):
+    """Always returns None, even if the key does not exist"""
+    return db_register.delete(username)
+
+
+
+admins = fetch_admin()
+usernames = [user["key"] for user in admins]
+names = [user["name"] for user in admins]
+hashed_passwords = [user["password"] for user in admins]
+
+
+authenticator = stauth.Authenticate(names, usernames, hashed_passwords,
+    "sales_dashboard", "abcdef", cookie_expiry_days=30)
+
+name, authentication_status, username = authenticator.login("Login", "main")
+
+if authentication_status == False:
+    st.error("Username/password is incorrect")
+
+if authentication_status == None:
+    st.warning("Please enter your username and password")
+
+if authentication_status:
+
+
+
+
+
+
+
+
+    selected3 = option_menu(None, ["User Registration", "User Chart Add"],
         
-        # Hash the password
-        hashed_password = pwd_context.hash(passs)
+        default_index=0, orientation="horizontal",
+        styles = {
+        "container": {
+            "padding": "1rem 2rem",
+            "background-color": "white",
+            "border-radius": "10px",
+            "box-shadow": "0 0 10px rgba(0, 0, 0, 0.1)",
+            "font-color": "white"
+        },
+        "icon": {
+            "color": "white",
+            "font-size": "25px",
+            "display": "none",
+            "font-color": "white"
+            
+        },
+        "nav-link": {
+            "font-size": "18px",
+            "font-color": "white",
+            "text-align": "left",
+            "padding": "0.5rem",
+            "margin": "0",
+            "border-radius": "5px",
+            "transition": "background-color 0.3s",
+            "--hover-color": "#e0e0e0",
+            "cursor": "pointer"
+        },
+        "nav-link-selected": {
+            "background-color": "#9C2222",
+            "color": "white",
+            "font-weight": "bold"
+        },
+    }
+        
+    )
 
-        # Now save the user information with the hashed password
-        insert_user(usernames, names, hashed_password)
- 
- 
+
+    def hash_password(password):
+        # Using hashlib to hash the password securely
+        return hashlib.sha256(password.encode()).hexdigest()
+
+    def insert_user(username, name, password):
+        """Returns the user on a successful user creation, otherwise raises and error"""
+        return db_register.put({"key": username, "name": username, "password": password})
+
+    def fetch_all_users():
+
+        res = db_register.fetch()
+        return res.items
 
 
+    DETA_KEY_REGISTER = "d0k4fws8gi7_xKE62N6R1cmQ9C9dKdHoEqpCPY6yfRN3"
+    deta_register = Deta(DETA_KEY_REGISTER)
+    db_register = deta_register.Base("users")
+    def insert_user(username, name, password):
+        """Returns the user on a successful user creation, otherwise raises and error"""
+        return db_register.put({"key": username, "name": name, "password": password})
+
+    users = fetch_all_users()
+    un = [user["key"] for user in users]
 
 
-if selected3=="User Chart Add":
-    st.header("Add user Chart's Url here")
-    un.insert(0, "Select a username")
+    if selected3 == "User Registration":
+        
 
-# Selectbox to choose a username
-    username_selected = st.selectbox('Select a username', un)
+        usernames = st.text_input("Username", key=9)
+        names = st.text_input("Name", key=10)
+        passs = st.text_input("Password", type="password", key=11)
+
+        if st.button("Save"):
+            
+            # Hash the password
+            hashed_password = pwd_context.hash(passs)
+
+            # Now save the user information with the hashed password
+            insert_user(usernames, names, hashed_password)
+
+
+        st.header("Delete User")
+        un.insert(0, "Select a username")
+
+    # Selectbox to choose a username
+        username_selected = st.selectbox('Select a username', un)
+
+        if st.button('Delete'):
+            delete_user(username_selected)
+
+    
     
 
-    url = st.text_input('Enter the URL:',key=4)
 
-    # "Add URL" button to add the URL to Deta
-    if st.button('Add URL'):
-        if username_selected.strip() and url.strip():
-            insert_url(username_selected, url)
-            st.success(f"URL added for user {username_selected}")
 
-    # "Fetch Data" button to display URLs for the entered username
-    if st.button('Fetch Data'):
-        if username_selected.strip():
-            user_data = fetch_user_data(username_selected)
-            if user_data:
-                urls = [user["url"] for user in user_data]
-                for url in urls:
-                    st.markdown(f'<div class="hover-shadow"><iframe src="{url}"></iframe></div>', unsafe_allow_html=True)
+
+    if selected3=="User Chart Add":
+        st.header("Add user Chart's Url here")
+        un.insert(0, "Select a username")
+
+    # Selectbox to choose a username
+        username_selected = st.selectbox('Select a username', un)
+        
+
+        url = st.text_input('Enter the URL:',key=4)
+
+        # "Add URL" button to add the URL to Deta
+        if st.button('Add URL'):
+            if username_selected.strip() and url.strip():
+                insert_url(username_selected, url)
+                st.success(f"URL added for user {username_selected}")
+
+        # "Fetch Data" button to display URLs for the entered username
+        if st.button('Fetch Data'):
+            if username_selected.strip():
+                user_data = fetch_user_data(username_selected)
+                if user_data:
+                    urls = [user["url"] for user in user_data]
+                    for url in urls:
+                        st.markdown(f'<div class="hover-shadow"><iframe src="{url}"></iframe></div>', unsafe_allow_html=True)
+                else:
+                    st.warning(f"No data found for user {username_selected}")
             else:
-                st.warning(f"No data found for user {username_selected}")
-        else:
-            st.warning("Please enter a valid username.")
-
+                st.warning("Please enter a valid username.")
 
 
